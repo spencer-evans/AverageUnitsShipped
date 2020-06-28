5fcb9216e95cecd204656fbe3620f74c5aab4653
spencernewellevans <spencernewellevans@gmail.com>
2020-06-27 21:34:30 -0400

    Added functionality for 3 employees

--- AverageUnitsShipped/Form1.Designer.vb
+++ AverageUnitsShipped/Form1.Designer.vb
@@ -26,18 +26,26 @@ Partial Class frmAverageUnitsShipped
         Me.lblUnits = New System.Windows.Forms.Label()
         Me.txtUnits = New System.Windows.Forms.TextBox()
         Me.lblDays = New System.Windows.Forms.Label()
-        Me.txtEntryDisplay = New System.Windows.Forms.TextBox()
-        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.txtEmployee1 = New System.Windows.Forms.TextBox()
+        Me.lblEmp1Average = New System.Windows.Forms.Label()
         Me.btnEnter = New System.Windows.Forms.Button()
         Me.btnReset = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.lblEmp2Average = New System.Windows.Forms.Label()
+        Me.lblEmp3Average = New System.Windows.Forms.Label()
+        Me.lblFinalAverage = New System.Windows.Forms.Label()
+        Me.txtEmployee2 = New System.Windows.Forms.TextBox()
+        Me.txtEmployee3 = New System.Windows.Forms.TextBox()
+        Me.lblEmployee1 = New System.Windows.Forms.Label()
+        Me.lblEmployee2 = New System.Windows.Forms.Label()
+        Me.lblEmployee3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblUnits
         '
         Me.lblUnits.AutoSize = True
-        Me.lblUnits.Location = New System.Drawing.Point(87, 22)
+        Me.lblUnits.Location = New System.Drawing.Point(19, 25)
         Me.lblUnits.Name = "lblUnits"
         Me.lblUnits.Size = New System.Drawing.Size(44, 17)
         Me.lblUnits.TabIndex = 0
@@ -45,48 +53,48 @@ Partial Class frmAverageUnitsShipped
         '
         'txtUnits
         '
-        Me.txtUnits.Location = New System.Drawing.Point(141, 19)
+        Me.txtUnits.Location = New System.Drawing.Point(69, 25)
         Me.txtUnits.Name = "txtUnits"
-        Me.txtUnits.Size = New System.Drawing.Size(100, 22)
+        Me.txtUnits.Size = New System.Drawing.Size(62, 22)
         Me.txtUnits.TabIndex = 1
         Me.ToolTip1.SetToolTip(Me.txtUnits, "Enter the number of units shipped")
         '
         'lblDays
         '
         Me.lblDays.AutoSize = True
-        Me.lblDays.Location = New System.Drawing.Point(247, 22)
+        Me.lblDays.Location = New System.Drawing.Point(361, 25)
         Me.lblDays.Name = "lblDays"
         Me.lblDays.Size = New System.Drawing.Size(45, 17)
         Me.lblDays.TabIndex = 2
         Me.lblDays.Text = "Day 1"
         '
-        'txtEntryDisplay
+        'txtEmployee1
         '
-        Me.txtEntryDisplay.Location = New System.Drawing.Point(67, 52)
-        Me.txtEntryDisplay.Multiline = True
-        Me.txtEntryDisplay.Name = "txtEntryDisplay"
-        Me.txtEntryDisplay.ReadOnly = True
-        Me.txtEntryDisplay.Size = New System.Drawing.Size(249, 163)
-        Me.txtEntryDisplay.TabIndex = 3
-        Me.txtEntryDisplay.TabStop = False
-        Me.txtEntryDisplay.WordWrap = False
+        Me.txtEmployee1.Location = New System.Drawing.Point(21, 95)
+        Me.txtEmployee1.Multiline = True
+        Me.txtEmployee1.Name = "txtEmployee1"
+        Me.txtEmployee1.ReadOnly = True
+        Me.txtEmployee1.Size = New System.Drawing.Size(142, 163)
+        Me.txtEmployee1.TabIndex = 4
+        Me.txtEmployee1.TabStop = False
+        Me.txtEmployee1.WordWrap = False
         '
-        'lblOutput
+        'lblEmp1Average
         '
-        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblOutput.Location = New System.Drawing.Point(66, 218)
-        Me.lblOutput.Name = "lblOutput"
-        Me.lblOutput.Size = New System.Drawing.Size(250, 31)
-        Me.lblOutput.TabIndex = 4
-        Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        Me.ToolTip1.SetToolTip(Me.lblOutput, "Average Units Shipped")
+        Me.lblEmp1Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmp1Average.Location = New System.Drawing.Point(21, 266)
+        Me.lblEmp1Average.Name = "lblEmp1Average"
+        Me.lblEmp1Average.Size = New System.Drawing.Size(142, 31)
+        Me.lblEmp1Average.TabIndex = 5
+        Me.lblEmp1Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lblEmp1Average, "Average Units Shipped")
         '
         'btnEnter
         '
-        Me.btnEnter.Location = New System.Drawing.Point(55, 276)
+        Me.btnEnter.Location = New System.Drawing.Point(127, 354)
         Me.btnEnter.Name = "btnEnter"
         Me.btnEnter.Size = New System.Drawing.Size(75, 29)
-        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.TabIndex = 13
         Me.btnEnter.Text = "&Enter"
         Me.ToolTip1.SetToolTip(Me.btnEnter, "Enter Data")
         Me.btnEnter.UseVisualStyleBackColor = True
@@ -94,10 +102,10 @@ Partial Class frmAverageUnitsShipped
         'btnReset
         '
         Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnReset.Location = New System.Drawing.Point(152, 275)
+        Me.btnReset.Location = New System.Drawing.Point(216, 353)
         Me.btnReset.Name = "btnReset"
         Me.btnReset.Size = New System.Drawing.Size(75, 30)
-        Me.btnReset.TabIndex = 6
+        Me.btnReset.TabIndex = 14
         Me.btnReset.Text = "&Reset"
         Me.ToolTip1.SetToolTip(Me.btnReset, "Clear form and start over")
         Me.btnReset.UseVisualStyleBackColor = True
@@ -105,13 +113,93 @@ Partial Class frmAverageUnitsShipped
         'btnExit
         '
         Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnExit.Location = New System.Drawing.Point(249, 275)
+        Me.btnExit.Location = New System.Drawing.Point(305, 353)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(75, 29)
-        Me.btnExit.TabIndex = 7
+        Me.btnExit.TabIndex = 15
         Me.btnExit.Text = "E&xit"
         Me.ToolTip1.SetToolTip(Me.btnExit, "End application")
         Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblEmp2Average
+        '
+        Me.lblEmp2Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmp2Average.Location = New System.Drawing.Point(182, 266)
+        Me.lblEmp2Average.Name = "lblEmp2Average"
+        Me.lblEmp2Average.Size = New System.Drawing.Size(142, 31)
+        Me.lblEmp2Average.TabIndex = 8
+        Me.lblEmp2Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lblEmp2Average, "Average Units Shipped")
+        '
+        'lblEmp3Average
+        '
+        Me.lblEmp3Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmp3Average.Location = New System.Drawing.Point(343, 266)
+        Me.lblEmp3Average.Name = "lblEmp3Average"
+        Me.lblEmp3Average.Size = New System.Drawing.Size(142, 31)
+        Me.lblEmp3Average.TabIndex = 11
+        Me.lblEmp3Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lblEmp3Average, "Average Units Shipped")
+        '
+        'lblFinalAverage
+        '
+        Me.lblFinalAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFinalAverage.Location = New System.Drawing.Point(22, 306)
+        Me.lblFinalAverage.Name = "lblFinalAverage"
+        Me.lblFinalAverage.Size = New System.Drawing.Size(463, 31)
+        Me.lblFinalAverage.TabIndex = 12
+        Me.lblFinalAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lblFinalAverage, "Average Units Shipped")
+        '
+        'txtEmployee2
+        '
+        Me.txtEmployee2.Location = New System.Drawing.Point(182, 95)
+        Me.txtEmployee2.Multiline = True
+        Me.txtEmployee2.Name = "txtEmployee2"
+        Me.txtEmployee2.ReadOnly = True
+        Me.txtEmployee2.Size = New System.Drawing.Size(142, 163)
+        Me.txtEmployee2.TabIndex = 7
+        Me.txtEmployee2.TabStop = False
+        Me.txtEmployee2.WordWrap = False
+        '
+        'txtEmployee3
+        '
+        Me.txtEmployee3.Location = New System.Drawing.Point(343, 95)
+        Me.txtEmployee3.Multiline = True
+        Me.txtEmployee3.Name = "txtEmployee3"
+        Me.txtEmployee3.ReadOnly = True
+        Me.txtEmployee3.Size = New System.Drawing.Size(142, 163)
+        Me.txtEmployee3.TabIndex = 10
+        Me.txtEmployee3.TabStop = False
+        Me.txtEmployee3.WordWrap = False
+        '
+        'lblEmployee1
+        '
+        Me.lblEmployee1.AutoSize = True
+        Me.lblEmployee1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmployee1.Location = New System.Drawing.Point(51, 71)
+        Me.lblEmployee1.Name = "lblEmployee1"
+        Me.lblEmployee1.Size = New System.Drawing.Size(92, 17)
+        Me.lblEmployee1.TabIndex = 3
+        Me.lblEmployee1.Text = "Employee 1"
+        '
+        'lblEmployee2
+        '
+        Me.lblEmployee2.AutoSize = True
+        Me.lblEmployee2.Location = New System.Drawing.Point(212, 70)
+        Me.lblEmployee2.Name = "lblEmployee2"
+        Me.lblEmployee2.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployee2.TabIndex = 6
+        Me.lblEmployee2.Text = "Employee 2"
+        '
+        'lblEmployee3
+        '
+        Me.lblEmployee3.AutoSize = True
+        Me.lblEmployee3.Location = New System.Drawing.Point(373, 70)
+        Me.lblEmployee3.Name = "lblEmployee3"
+        Me.lblEmployee3.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployee3.TabIndex = 9
+        Me.lblEmployee3.Text = "Employee 3"
         '
         'frmAverageUnitsShipped
         '
@@ -119,12 +207,20 @@ Partial Class frmAverageUnitsShipped
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.btnReset
-        Me.ClientSize = New System.Drawing.Size(378, 325)
+        Me.ClientSize = New System.Drawing.Size(506, 407)
+        Me.Controls.Add(Me.lblEmployee3)
+        Me.Controls.Add(Me.lblEmployee2)
+        Me.Controls.Add(Me.lblEmployee1)
+        Me.Controls.Add(Me.lblFinalAverage)
+        Me.Controls.Add(Me.lblEmp3Average)
+        Me.Controls.Add(Me.lblEmp2Average)
+        Me.Controls.Add(Me.txtEmployee3)
+        Me.Controls.Add(Me.txtEmployee2)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnReset)
         Me.Controls.Add(Me.btnEnter)
-        Me.Controls.Add(Me.lblOutput)
-        Me.Controls.Add(Me.txtEntryDisplay)
+        Me.Controls.Add(Me.lblEmp1Average)
+        Me.Controls.Add(Me.txtEmployee1)
         Me.Controls.Add(Me.lblDays)
         Me.Controls.Add(Me.txtUnits)
         Me.Controls.Add(Me.lblUnits)
@@ -143,10 +239,18 @@ Partial Class frmAverageUnitsShipped
     Friend WithEvents lblUnits As Label
     Friend WithEvents txtUnits As TextBox
     Friend WithEvents lblDays As Label
-    Friend WithEvents txtEntryDisplay As TextBox
-    Friend WithEvents lblOutput As Label
+    Friend WithEvents txtEmployee1 As TextBox
+    Friend WithEvents lblEmp1Average As Label
     Friend WithEvents btnEnter As Button
     Friend WithEvents btnReset As Button
     Friend WithEvents btnExit As Button
     Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents txtEmployee2 As TextBox
+    Friend WithEvents txtEmployee3 As TextBox
+    Friend WithEvents lblEmp2Average As Label
+    Friend WithEvents lblEmp3Average As Label
+    Friend WithEvents lblFinalAverage As Label
+    Friend WithEvents lblEmployee1 As Label
+    Friend WithEvents lblEmployee2 As Label
+    Friend WithEvents lblEmployee3 As Label
 End Class
--- AverageUnitsShipped/Form1.vb
+++ AverageUnitsShipped/Form1.vb
@@ -1,45 +1,153 @@
-﻿Public Class frmAverageUnitsShipped
-    Dim currentDay As Integer = 1
-    Dim totalUnits As Integer = 0
+﻿''' <summary>
+''' AverageUnitsShipped
+''' Author: Spencer Evans 100777371
+''' Date: 2020-06-07
+''' Description: This application calculates the weekly average units shipped for 3 employees and overall average
+''' units shipped for the company.
+'''
+''' </summary>
 
+Public Class frmAverageUnitsShipped
+    Dim day As Integer = 1          ' tracks the day that data is being input for
+    Dim d As Integer                ' array index
+    Dim employee As Integer = 1     ' Tracks the employee that data is being input for
+    Dim emp As Integer              ' array index
+    Dim units(2, 6) As Integer      ' 2D array that stores the units shipped per day per employee
+    Dim averages(3) As Double       ' Stores the weekly average shipped units for each employee
+    Dim averagesTotal As Double = 0 ' Sum of employee averages for overall company average calculation
+
+    ''' <summary>
+    ''' Event handler for "Enter" button click. Input is validated and displayed in the appropriate textbox.
+    ''' Averages for each employee and displayed in their respective labels. The overall average is also displayed.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
-        Dim units As Integer
 
+        emp = employee - 1
+        d = day - 1
 
-        If Integer.TryParse(txtUnits.Text, units) Then
-            If units >= 0 And units <= 5000 Then
-                totalUnits += units
-                txtEntryDisplay.AppendText(String.Format("{0}", units & Environment.NewLine))
-                currentDay += 1
+        ' Sets the font style to bold for the employee that data is being entered for
+        If employee = 1 Then
+            lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Bold)
+            lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Regular)
+            lblEmployee3.Font = New Font(lblEmployee3.Font, FontStyle.Regular)
+        ElseIf employee = 2 Then
+            lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Regular)
+            lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Bold)
+            lblEmployee3.Font = New Font(lblEmployee3.Font, FontStyle.Regular)
+        ElseIf employee = 3 Then
+            lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Regular)
+            lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Regular)
+            lblEmployee3.Font = New Font(lblEmployee3.Font, FontStyle.Bold)
+        End If
+
+        ' Data validation 
+        ' Only whole number input is allowed. Valid inputs are stored in units array
+        If Integer.TryParse(txtUnits.Text, units(emp, d)) Then
+            ' Inputs must be between 0 and 5000, inclusive
+            If units(emp, d) >= 0 And units(emp, d) <= 5000 Then
+                ' If input is valid display is employee's textbox and increments day
+                If employee = 1 Then
+                    txtEmployee1.AppendText(String.Format("{0}", units(emp, d) & Environment.NewLine))
+                ElseIf employee = 2 Then
+                    txtEmployee2.AppendText(String.Format("{0}", units(emp, d) & Environment.NewLine))
+                Else
+                    txtEmployee3.AppendText(String.Format("{0}", units(emp, d) & Environment.NewLine))
+                End If
+                day += 1
             Else
+                ' Error message for out of range input
                 MessageBox.Show("Input must be between 0 and 5000")
             End If
         Else
+            ' Error message for non-numeric input
             MessageBox.Show("Input must be numeric")
         End If
 
-        If currentDay = 8 Then
-            lblOutput.Text = String.Format("Average units per day: {0}", FormatNumber(totalUnits / 7, 2))
-            txtUnits.ReadOnly = True
-            btnEnter.Enabled = False
+        ' Outputs averages to appropriate textboxes
+        ' Final average is only calculated once input is made for third employee on after final day
+        If day = 8 And employee = 3 Then
+            ' Sums all inputs for employee 3
+            For i As Integer = 0 To 6
+                averages(2) += units(2, i)
+            Next
+            averages(2) = averages(2) / 7
+            ' Employee 3 weekly average output
+            lblEmp3Average.Text = String.Format("Average: {0}", FormatNumber(averages(2), 2))
+            ' Sums all averages
+            For i As Integer = 0 To 2
+                averagesTotal += averages(i)
+            Next
+            ' Overall average output
+            lblFinalAverage.Text = String.Format("Average units per day: {0}", FormatNumber(averagesTotal / 3, 2))
+            txtUnits.ReadOnly = True ' Disables input
+            btnEnter.Enabled = False ' Disables enter button
+            ' After final day for employees 1 and 2 the averages are calculated and displayed
+        ElseIf day = 8 Then
+            If employee = 1 Then
+                ' Sums all inputs for employee 1
+                For i As Integer = 0 To 6
+                    averages(0) += units(0, i)
+                Next
+                averages(0) = averages(0) / 7
+                ' Employee 1 weekly average output
+                lblEmp1Average.Text = String.Format("Average: {0}", FormatNumber(averages(0), 2))
+            ElseIf employee = 2 Then
+                ' Sums all inputs for employee 2
+                For i As Integer = 0 To 6
+                    averages(1) += units(1, i)
+                Next
+                averages(1) = averages(1) / 7
+                ' Employee 2 weekly average output
+                lblEmp2Average.Text = String.Format("Average: {0}", FormatNumber(averages(1), 2))
+            End If
+            employee += 1 ' Proceed to next employee after output is displayed
+            day = 1 ' Day reset to 1 after average is displayed
         Else
-            lblDays.Text = String.Format("Day {0}", currentDay)
+            ' Updates "Day" label
+            lblDays.Text = String.Format("Day {0}", day)
         End If
+        'Clears input textbox
         txtUnits.Text = ""
 
     End Sub
 
+    ''' <summary>
+    ''' Event handler for "Reset" button
+    ''' Clears all input and output from the form and re-enables user input
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
-        currentDay = 1
+        day = 1
+        employee = 1
+        For i As Integer = 0 To 2
+            averages(i) = 0
+        Next
+        averagesTotal = 0
         lblDays.Text = "Day 1"
         txtUnits.Text = ""
-        txtEntryDisplay.Text = ""
-        totalUnits = 0
-        lblOutput.Text = ""
+        txtEmployee1.Text = ""
+        txtEmployee2.Text = ""
+        txtEmployee3.Text = ""
+        lblEmp1Average.Text = ""
+        lblEmp2Average.Text = ""
+        lblEmp3Average.Text = ""
+        lblFinalAverage.Text = ""
         txtUnits.ReadOnly = False
         btnEnter.Enabled = True
+        lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Bold)
+        lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Regular)
+        lblEmployee3.Font = New Font(lblEmployee3.Font, FontStyle.Regular)
     End Sub
 
+    ''' <summary>
+    ''' Event handler for "Exit" button
+    ''' Closes the program
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Close()
     End Sub
